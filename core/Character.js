87a5edf76d45225a3e3845e1951477f551ff0d26
KellyRoseOwO <kellyroseuwu@gmail.com>
2025-12-07 15:09:15 +0100

    Update Character.js

--- core/Character.js
+++ core/Character.js
@@ -2,10 +2,21 @@ export class Character {
 
     constructor({
         colliding = false,
-        pickups = [false],
+        pickups = [],
     } = {}) {
         this.colliding = colliding;
         this.pickups = pickups;
     }
 
+    addItem(item) {
+        this.pickups.push(item);
+    }
+
+    getInventory() {
+        return this.pickups;
+    }
+
 }
+
+window.Character = new Character();
+export default window.Character;
